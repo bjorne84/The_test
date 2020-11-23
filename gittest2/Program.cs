59deeb2cf09a6dd47b4622a5950400b13924c954
bjorne84 <edinbjorn@gmail.com>
2020-11-23 21:26:38 +0100

    Add project files.

---
+++ gittest2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gittest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
